668122acdfefc6742277907b2afbe28581d14930
Tolga Uzundag <uzundag.tolga@hotmail.com>
2024-08-12 20:40:34 +0300

    Removal of some files
    Removed some unnecessary files.

--- testvar.py
+++
@@ -1,9 +0,0 @@
-import pyrebase
-import time
-from requests.packages.urllib3.contrib.appengine import is_appengine_sandbox
-
-
-print("Succes")
-#time.sleep(10)
-if is_appengine_sandbox():
-    pass
